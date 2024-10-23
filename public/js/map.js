92e22e21d7dcd516bc981ce43047b082288d0b04
Chaithanya-ks8 <kschaithanya48@gmail.com>
2024-10-23 16:47:52 +0530

    map.js updated

--- public/js/map.js
+++ public/js/map.js
@@ -5,12 +5,11 @@
 container: "map", // container ID
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
  style: "mapbox://styles/mapbox/streets-v12", // style URL
- center: [75.7139, 15.3173], // starting position [lng, lat]
+ center: coordinates, // starting position [lng, lat]
 zoom: 9,// starting zoom
     });
 
-console.log(coordinates);
 
     const marker = new mapboxgl.Marker()
-    .setLngLat([coordinates]) //Listing.geometry.coordinates
+    .setLngLat(coordinates) //Listing.geometry.coordinates
     .addTo(map);
